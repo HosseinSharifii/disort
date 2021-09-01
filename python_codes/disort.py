26f2442912a62edcf421e861deddd00e6c3a6388
HosseinGit <42911295+HosseinGit@users.noreply.github.com>
2021-09-01 11:29:11 -0400

    sorted all dicom images

--- python_codes/disort.py
+++ python_codes/disort.py
@@ -4,44 +4,97 @@ Created on Mon Aug 30 19:45:20 2021
 @author: Hossein Sharifi
 """
 import os
-import json 
+import sys
 import pydicom
-import numpy as np
 import shutil
 
 
 
 def return_sorted_dicom(dir_str=[]):
+   
+    grand_parent_root = 0
+    parent_root = 0
+    child_root = 0
+    # handle the assigned str
+    grand_parent_root,parent_root,child_root = handle_roots(dir_str)
 
+    # now handle reading the files according to the assigned dir root 
+    if grand_parent_root == 1:
+        print('grand_parent_root is given')
+        for gf in os.listdir(dir_str):
+            if os.path.isdir(dir_str+'/'+gf):
+                parent_str =  dir_str+'/'+gf
+                for pf in os.listdir(parent_str):
+                    if (os.path.isdir(parent_str+'/'+pf) and len(pf.split('_')[0])==8):
+                        child_str = parent_str + '/' +pf
+                        handle_child_root(child_str)
+
+    elif parent_root == 1:
+        print('parent_root is given')
+        for pf in os.listdir(dir_str):
+            
+            if (os.path.isdir(dir_str+'/'+pf) and len(pf.split('_')[0])==8):
+                child_str = dir_str + '/' +pf
+                handle_child_root(child_str)
+
+    elif child_root == 1:
+        print('child_root is given')
+        child_str = dir_str
+        handle_child_root(child_str)
+
+
+def handle_roots(dir_str=""):
+    grand_parent_root = 0
+    parent_root = 0
+    child_root = 0
+    
+    # handle the assigned str
+    if dir_str.split('/')[-1] == 'MRI':
+        print('grand is activated')
+        grand_parent_root = 1
+    elif '-' in dir_str.split('/')[-1] and dir_str.split('/')[-1].split('-')[0].isnumeric():
+        print('parent is activated')
+        parent_root = 1 
+    else:
+        print('child is activated')
+        child_root = 1
+    return grand_parent_root , parent_root, child_root
+
+def handle_child_root(child_str = ""):
     # first create a list of image folders
-    files = os.listdir(dir_str)
-    sorted_dcm_str = dir_str + '/' + 'sorted_dcm'
+    print('Looking for dicom images in this directory to sort...')
+    print(child_str)
+    files = os.listdir(child_str)
+    sorted_dcm_str = child_str + '/' + 'sorted_dcm'
     if not 'sorted_dcm' in files:
         os.makedirs(sorted_dcm_str)
+    else:
+        print('sorted dicom directory is already existed')
+        return
+
     img_dir_list = []
     for f in files:
-        p = dir_str + '/' + f
+        p = child_str + '/' + f
         if (f.isnumeric() and  os.path.isdir(p)):
-            print(f)
             img_dir_list.append(f)
         
     img_dir_list.sort(key=int)
     #print(img_dir_list)
-    #print(os.getcwd())
 
     # Start opening each folder and 
     # try to search for the dcm file
     for f in img_dir_list:
-        temp_str = dir_str+'/'+f
+        temp_str = child_str+'/'+f
         if 'pdata' in os.listdir(temp_str):
             temp_str = temp_str + '/'+'pdata'
             if '1' in os.listdir(temp_str):
                 temp_str = temp_str+'/'+'1'
-                if 'dicom' in os.listdir(temp_str):
+                if 'dicom' in os.listdir(temp_str) and 'EnIm1.dcm' in os.listdir(temp_str + '/' + 'dicom'):
                     dcm_str = temp_str + '/' + 'dicom' + '/' + 'EnIm1.dcm'
                     dcm_dataset = pydicom.dcmread(dcm_str)
                     #print(dcm_dataset.dir())
                     img_name = dcm_dataset['SeriesDescription'].value
+                    #print(img_name)
                     if img_name in ['SA_1','SA_2','SA_3','SA_4',
                                     'SA_5','SA_6','LA_2CH','LA_PLUS_60',
                                     'LA_PLUS60','LA_MINUS_60','LA_MINUS60']:
@@ -49,7 +102,20 @@ def return_sorted_dicom(dir_str=[]):
                         shutil.copy(dcm_str, new_img_str)
                         print(dcm_dataset['SeriesDescription'].value)
 
-
 if __name__ == '__main__':
-    dir_str = '/Users/hosseinsharifi/Downloads/test_dicom'
-    return_sorted_dicom(dir_str)
+
+    no_of_arguments = len(sys.argv)
+    print(no_of_arguments)
+    
+    if no_of_arguments == 1:
+        print('No directory has been assigned!')
+        print('Exiting...')
+    elif no_of_arguments == 2:
+        print('Start sorting dicom images in the following path directory ...')
+        print(sys.argv[1])
+        dir_str = sys.argv[1]
+        dir_str = dir_str.replace('\\','/')
+        print(dir_str)
+
+
+        return_sorted_dicom(dir_str)
